1a46d44c8009bde2af243460fb814713e303b27b
John A. Pérez R <japr893@gmail.com>
2023-04-26 12:32:45 -0400

    Fix span around icon to increase navbar icons size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       <button type="button" onClick={customFunc} style={{ color }}
         className="relative text-xl rounded-full p-3 hover:bg-light-gray">
           <span style={{ background: dotColor }}
-          className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
-            {icon}
-          </span>
+            className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"/>
+          {icon}
       </button>
   </TooltipComponent>
 )
