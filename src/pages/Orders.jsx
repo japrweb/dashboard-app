dc71843c471f28d8e8f578841bc7f86e94a7747b
John A. Pérez R <japr893@gmail.com>
2023-04-27 18:34:16 -0400

    Orders sorting & more mt in mobile

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -6,12 +6,13 @@ import { Header } from '../components';
 
 const Orders = () => {
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <div className="m-2 mt-16 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
       <GridComponent
         id="gridcomp"
         dataSource={ordersData}
         allowPaging
+        allowSorting
       >
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
