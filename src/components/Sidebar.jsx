4368a8e16787d04aa0d7c96dd37a59133fa6ea7f
John A. Pérez R <japr893@gmail.com>
2023-04-28 12:51:15 -0400

    Customers page components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -26,7 +26,7 @@ const Sidebar = () => {
       {activeMenu && (<>
         <div className="flex justify-between items-center">
           <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-            <SiShopware/> <span>Dashboard</span>
+            <SiShopware/> <span>Dashboard App</span>
           </Link>
           <TooltipComponent content="Menu" position="BottomCenter">
             <button type="button"
