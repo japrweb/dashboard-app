57e1fdf3cf72e9c3e974670fe59b2b1f62a3dbb3
John A. Pérez R <japr893@gmail.com>
2023-04-28 13:12:40 -0400

    Calendar app imports

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { ScheduleComponent, ViewsDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
+
+import { scheduleData } from '../data/dummy';
 
 const Calendar = () => {
   return (
