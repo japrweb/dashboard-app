4d80e3004cfd5b264196ff39eaf72f08d6334f0d
John A. Pérez R <japr893@gmail.com>
2023-05-01 18:04:04 -0400

    Theme settings sidebar options

--- src/App.js
+++ src/App.js
@@ -43,6 +43,8 @@ const App = () => {
             </div>
 
             <div>
+              <ThemeSettings />
+
               <Routes>
                 {/* Dashboard */}
                 <Route path="/" element={<Ecommerce/>}/>
