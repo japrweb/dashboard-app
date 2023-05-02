9bd5d72581e7cac21ba0f64e85ccf6adcf7179bc
John A. Pérez R <japr893@gmail.com>
2023-05-02 12:02:28 -0400

    Settings options dark mode

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 const App = () => {
-  const { activeMenu, themeSettings, setThemeSettings, currentColor } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext();
 
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
@@ -40,7 +40,10 @@ const App = () => {
           )}
           
           <div className={
-          `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+            `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full 
+            ${activeMenu 
+              ? 'md:ml-72' 
+              : 'flex-2'}`
           }>
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,8 +18,8 @@ const Sidebar = () => {
     }
   }
   
-  const activeLink= 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
-  const normalLink= 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 drak:hover:text-black hover:bg-light-gray m-2';
+  const activeLink= 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 font-semibold';
+  const normalLink= 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md hover:font-semibold text-gray-700 dark:text-gray-200 dark:hover:text-black dark:hover:font-semibold hover:bg-light-gray m-2';
 
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
