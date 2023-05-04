4a7e5d44ac05edea49cd3b8fc124a40c48872285
John A. Pérez R <japr893@gmail.com>
2023-05-04 11:20:47 -0400

    Pie chart ecommerce

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -146,8 +146,15 @@ const Ecommerce = () => {
             </div>
           </div>
 
-          <div>
-            
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-400 p-8 m-3 flex justify-center items-center gap-10">
+            <div>
+              <p className="text-2xl font-semibold">$43,246</p>
+              <p className="text-gray-400">Yearly sales</p>
+            </div>
+
+            <div className="w-40">
+              <Pie id="pie-chart" data={ecomPieChartData} legendVisiblity={false} height="160px" />
+            </div>
           </div>
         </div>
       </div>
