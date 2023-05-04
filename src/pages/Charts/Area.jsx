e1812ee6e4d27f6253081fdf60965f429267c0c2
John A. Pérez R <japr893@gmail.com>
2023-05-04 13:28:37 -0400

    Area chart dark mode

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, SplineAreaSeries, DateTime, Legend } from '@syncfusion/ej2-react-charts';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, SplineAreaSeries, Legend } from '@syncfusion/ej2-react-charts';
 
 import { ChartsHeader } from '../../components';
-import { areaCustomSeries, areaPrimaryYAxis, areaPrimaryXAxis } from '../../data/dummy';
+import { areaCustomSeries, areaPrimaryXAxis, areaPrimaryYAxis } from '../../data/dummy';
 import { useStateContext } from '../../contexts/ContextProvider';
-
 
 const Area = () => {
   const { currentMode } = useStateContext();
@@ -12,23 +11,24 @@ const Area = () => {
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <ChartsHeader category="Area" title="Inflation Rate in percentage" />
-      <ChartComponent
-        id="area-chart"
-        height="420px"
-        primaryYAxis={areaPrimaryYAxis}
-        primaryXAxis={areaPrimaryXAxis}
-        chartArea={{ border: { width: 0 } }}
-        tooltip={{ enable: true }}
-        background={currentMode === 'Dark' ? '#fff' : '#fff'}
-      >
-        <Inject services={[SplineAreaSeries, DateTime, Legend]} />
-        <SeriesCollectionDirective>
-          {areaCustomSeries.map((item, index) => 
-          <SeriesDirective key={index}{...item} />)}
-        </SeriesCollectionDirective>
-      </ChartComponent>
+      <div className="w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={areaPrimaryXAxis}
+          primaryYAxis={areaPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+          legendSettings={{ background: 'white' }}
+        >
+          <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+          <SeriesCollectionDirective>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            {areaCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Area
+export default Area;
