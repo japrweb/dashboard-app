65a1b3a24303c265086f5e65d4e8f30711685ddb
John A. Pérez R <japr893@gmail.com>
2023-05-04 13:02:52 -0400

    Daily Activities

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -303,17 +303,40 @@ const Ecommerce = () => {
           </div>
         </div>
 
-        <div className="w-400 bg-white ">
-          <div>
-            <p>Daily Activities</p>
-            <button>
+        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">Daily Activities</p>
+            <button type="button" className="text-xl font-semibold text-gray-500">
               <IoIosMore />
             </button>
+          </div>
+          <div className="mt-10">
+                <img
+                  className="md:w-96 h-50" 
+                  src={product9} 
+                  alt="" 
+                />
+                <div className="mt-8">
+                  <p className="font-semibold text-lg">React 18 coming soon!</p>
+                  <p className="text-gray-400">By Johnathan Doe</p>
+                  <p className="mt-8 text-sm text-gray-400">
+                    This will be the small description for the news you have shown here. There could be some great info.
+                  </p>
+                  <div className="mt-3">
+                    <Button
+                      color="white"
+                      bgColor={currentColor}
+                      text="Read More"
+                      borderRadius="10px"
+                      width="full"
+                    />
+                  </div>
+                </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Ecommerce
+export default Ecommerce;
